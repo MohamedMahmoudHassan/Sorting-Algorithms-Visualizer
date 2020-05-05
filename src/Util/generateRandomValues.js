b801ffe04a087ede383dfdcf6facb3023ca534fb
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-06 01:30:48 +0200

    Get random values fn really generate random values

--- src/Util/generateRandomValues.js
+++ src/Util/generateRandomValues.js
@@ -1,7 +1,7 @@
 export default function generateRandomValues(length) {
   let values = [];
   while (length--) {
-    values.push(100);
+    values.push(Math.floor(Math.random(1) * 200 + 40));
   }
   return values;
 }
