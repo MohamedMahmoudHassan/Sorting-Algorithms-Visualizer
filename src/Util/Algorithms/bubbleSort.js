34d5d4be8977ec8582727774a88c9cd68324fb31
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-06 23:43:45 +0200

    Sort steps need to know the step id

--- src/Util/Algorithms/bubbleSort.js
+++ src/Util/Algorithms/bubbleSort.js
@@ -1,13 +1,14 @@
 export default function bubbleSort(arr) {
   let sortSteps = [];
   const length = arr.length;
+  let c = 0;
 
   for (var i = 0; i < length - 1; i++) {
     for (var j = 0; j < length - 1; j++) {
-      sortSteps.push({ id1: j, id2: j + 1, type: "compare" });
+      sortSteps.push({ id: c++, el1: j, el2: j + 1, type: "compare" });
       if (arr[j + 1] < arr[j]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-        sortSteps.push({ id1: j, id2: j + 1, type: "swap" });
+        sortSteps.push({ id: c++, el1: j, el2: j + 1, type: "swap" });
       }
     }
   }
