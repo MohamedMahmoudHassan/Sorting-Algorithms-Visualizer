2e3750cdc3784ec10b6d21ae31f4c8bd5a2f147f
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-07 13:25:40 +0200

    App comp has state with all variables

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Array from "./components/array";
 import generateRandomElements from "./Util/generateRandomElements";
 
-function App() {
-  const arrayElements = generateRandomElements(50);
+class App extends Component {
+  state = { sortInterval: 0, sortAlgorithm: "", arrayElements: [] };
 
-  return (
-    <div className="container">
-      <h1>Sorting Algorithms Visualizer</h1>
-      <Array sortInterval={5} sortAlgorithm="selectionSort" elements={arrayElements} />
-    </div>
-  );
+  constructor(){
+    super();
+    this.state = {
+      sortInterval:10,
+      sortAlgorithm: "selectionSort",
+      arrayElements: generateRandomElements(50)
+    }
+  }
+
+  render() {
+    const {sortInterval, sortAlgorithm, arrayElements } = this.state;
+    return (
+      <div className="container">
+        <h1>Sorting Algorithms Visualizer</h1>
+        <Array sortInterval={sortInterval} sortAlgorithm={sortAlgorithm} elements={arrayElements} />
+      </div>
+    );
+  }
 }
 
 export default App;
