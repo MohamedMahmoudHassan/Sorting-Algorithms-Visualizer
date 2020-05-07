8619c2c169b6f21a86ff1ede42b5552e02575ffd
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-07 08:40:36 +0200

    Bubble sort bug fixed

--- src/Util/Algorithms/bubbleSort.js
+++ src/Util/Algorithms/bubbleSort.js
@@ -4,7 +4,7 @@ export default function bubbleSort(arr) {
   let c = 0;
 
   for (var i = 0; i < length - 1; i++) {
-    for (var j = 0; j < length - 1; j++) {
+    for (var j = 0; j < length - i - 1; j++) {
       sortSteps.push({ id: c++, el1: j, el2: j + 1, type: "compare" });
       if (arr[j + 1].value < arr[j].value) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
