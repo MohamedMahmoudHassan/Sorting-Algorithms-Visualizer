814f08f70f7a9b613fad83751e5ea0fc8fad06ab
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-08 10:03:28 +0200

    Edit selection sort to return list of arrays

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     super();
     this.state = {
       sortInterval: 10,
-      sortAlgorithm: "bubbleSort",
+      sortAlgorithm: "selectionSort",
       arrayElements: generateRandomElements(50)
     };
   }
--- src/Util/Algorithms/selectionSort.js
+++ src/Util/Algorithms/selectionSort.js
@@ -1,3 +1,5 @@
+import getSortStep from "./../getSortStep";
+
 export default function selectionSort(arr) {
   let sortSteps = [];
   const length = arr.length;
@@ -7,11 +9,11 @@ export default function selectionSort(arr) {
   for (var i = 0; i < length - 1; i++) {
     minIndex = i;
     for (var j = i + 1; j < length; j++) {
-      sortSteps.push({ id: c++, el1: minIndex, el2: j, type: "compare" });
+      sortSteps.push(getSortStep(arr, "compare", minIndex, j));
       if (arr[j].value < arr[minIndex].value) minIndex = j;
     }
     [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-    sortSteps.push({ id: c++, el1: i, el2: minIndex, type: "swap" });
+    sortSteps.push(getSortStep(arr, "swap", i, minIndex));
   }
   return sortSteps;
 }
