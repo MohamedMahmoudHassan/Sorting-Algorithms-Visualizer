497579c607d7d3e2eb96801dddf149758fee1423
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-08 11:04:40 +0200

    Lifiting up array state

--- src/App.js
+++ src/App.js
@@ -2,25 +2,72 @@ import React, { Component } from "react";
 import "./App.css";
 import Array from "./components/array";
 import generateRandomElements from "./Util/generateRandomElements";
+import sortWithSteps from "./Util/sortWithSteps";
 
 class App extends Component {
-  state = { sortInterval: 0, sortAlgorithm: "", arrayElements: [] };
+  state = {
+    array: { elements: [], sortSteps: [], currentStepId: 0, status: "" },
+    sortInterval: 0,
+    sortAlgorithm: ""
+  };
 
   constructor() {
     super();
+    const arrayElements = generateRandomElements(15);
     this.state = {
+      array: {
+        elements: arrayElements,
+        status: "unsorted",
+        currentStepId: 0,
+        sortSteps: [arrayElements]
+      },
       sortInterval: 10,
-      sortAlgorithm: "selectionSort",
-      arrayElements: generateRandomElements(50)
+      sortAlgorithm: "selectionSort"
     };
   }
 
+  componentDidUpdate() {
+    clearInterval(this.sortInterval);
+    if (this.state.array.status === "sorting")
+      this.sortInterval = setInterval(() => {
+        this.handleStatusUpdate();
+      }, this.state.sortInterval);
+  }
+
+  updateArrayState = arrayState => {
+    const array = { ...this.state.array };
+    Object.keys(arrayState).forEach(key => (array[key] = arrayState[key]));
+    this.setState({ array });
+  };
+
+  handleStatusUpdate = () => {
+    let { sortSteps, currentStepId, status } = this.state.array;
+    if (currentStepId + 1 === sortSteps.length) {
+      status = "sorted";
+    } else currentStepId++;
+
+    this.updateArrayState({ status, currentStepId });
+  };
+
+  handleSortStart = () => {
+    const { elements, status } = this.state.array;
+    if (status === "unsorted") {
+      const sortSteps = [elements, ...sortWithSteps([...elements], this.state.sortAlgorithm)];
+      this.updateArrayState({ status: "sorting", sortSteps });
+    }
+  };
+
   render() {
-    const { sortInterval, sortAlgorithm, arrayElements } = this.state;
+    const { sortInterval, sortAlgorithm, array } = this.state;
     return (
       <div className="container">
         <h1>Sorting Algorithms Visualizer</h1>
-        <Array sortInterval={sortInterval} sortAlgorithm={sortAlgorithm} elements={arrayElements} />
+        <Array
+          sortInterval={sortInterval}
+          sortAlgorithm={sortAlgorithm}
+          onClick={this.handleSortStart}
+          elements={array.sortSteps[array.currentStepId]}
+        />
       </div>
     );
   }
--- src/components/array.jsx
+++ src/components/array.jsx
@@ -1,39 +1,7 @@
 import React, { Component } from "react";
 import Bar from "./bar";
-import sortWithSteps from "./../Util/sortWithSteps";
 
 class Array extends Component {
-  state = { elements: [], sortSteps: [], currentStepId: 0, status: "" };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      elements: this.props.elements,
-      status: "unsorted",
-      currentStepId: 0,
-      sortSteps: [this.props.elements]
-    };
-  }
-
-  componentDidUpdate() {
-    clearInterval(this.sortInterval);
-    if (this.state.status !== "sorting") return;
-
-    this.sortInterval = setInterval(() => {
-      this.handleStatusUpdate();
-    }, this.props.sortInterval);
-  }
-
-  handleStatusUpdate = () => {
-    let { sortSteps, currentStepId, status } = this.state;
-
-    if (currentStepId + 1 === sortSteps.length) {
-      status = "sorted";
-    } else currentStepId++;
-
-    this.setState({ status, currentStepId });
-  };
-
   style = {
     width: 1000,
     height: 500,
@@ -41,23 +9,11 @@ class Array extends Component {
     borderStyle: "solid"
   };
 
-  handleClick = () => {
-    const status = this.state.status;
-    if (status === "unsorted") {
-      const sortSteps = [
-        this.state.elements,
-        ...sortWithSteps([...this.state.elements], this.props.sortAlgorithm)
-      ];
-      this.setState({ status: "sorting", sortSteps });
-    }
-  };
-
   render() {
-    const { sortSteps, currentStepId } = this.state;
     return (
-      <div className="row" style={this.style} onClick={this.handleClick}>
-        {sortSteps[currentStepId].map(element => (
-          <Bar maxValue={500} element={element} barsNumber={this.state.elements.length} />
+      <div className="row" style={this.style} onClick={this.props.onClick}>
+        {this.props.elements.map(element => (
+          <Bar maxValue={500} element={element} barsNumber={this.props.elements.length} />
         ))}
       </div>
     );
