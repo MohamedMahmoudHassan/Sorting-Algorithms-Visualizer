b14f39630fec932f856df5a4632a6068727781da
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-09 18:28:29 +0200

    Add css files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+import "./assets/css/argon-design-system-react.css"
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
