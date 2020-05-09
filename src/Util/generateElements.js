c8f86f30071c7cabcf4e4c17f5957958e5eedf09
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-09 09:24:45 +0200

    generateElements fn - type -> initialOrder

--- src/Util/generateElements.js
+++ src/Util/generateElements.js
@@ -1,7 +1,7 @@
 import random from "./Algorithms/Generator/random";
 import reversed from "./Algorithms/Generator/reversed";
 
-export default function generateElements(length, type, min = 5, max = 400) {
-  if (type === "Random") return random(length, min, max);
-  if (type === "Reversed") return reversed(length, min, max);
+export default function generateElements(length, initialOrder, min = 5, max = 400) {
+  if (initialOrder === "Random") return random(length, min, max);
+  if (initialOrder === "Reversed") return reversed(length, min, max);
 }
