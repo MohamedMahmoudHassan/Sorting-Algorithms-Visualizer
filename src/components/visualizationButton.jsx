1d301a0cf4ddfac8b69bc8a38a0629db05fdb550
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-10 16:54:21 +0200

    VisualizationButton is more elegant

--- src/components/visualizationButton.jsx
+++ src/components/visualizationButton.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 
-const VisualizationButton = ({ role, size, handleClick }) => {
-  return (
-    <i className={`fa fa-${role}-circle  fa-${size}x`} aria-hidden="true" onClick={handleClick} />
-  );
-};
+class VisualizationButton extends Component {
+  state = { color: "" };
+
+  constructor(props) {
+    super(props);
+    this.state = { style: { color: "#172b4d" } };
+  }
+
+  handleMouseEnter = () => {
+    this.setState({ style: { color: "#f5365c" } });
+  };
+
+  handleMouseOut = () => {
+    this.setState({ style: { color: "#172b4d" } });
+  };
+
+  render() {
+    const { size, role, handleClick, ...rest } = this.props;
+    return (
+      <span
+        className={`fa-stack fa-lg fa-${size}x`}
+        onClick={handleClick}
+        onMouseOut={this.handleMouseOut}
+        onMouseEnter={this.handleMouseEnter}
+        {...rest}
+      >
+        <i className={`fa fa-circle fa-stack-2x `} style={this.state.style} />
+        <i className={`fa fa-${role} fa-stack-1x`} style={{ color: "white" }} />
+      </span>
+    );
+  }
+}
 
 export default VisualizationButton;
