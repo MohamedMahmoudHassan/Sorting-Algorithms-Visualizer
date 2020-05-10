f7708e494cb9c628c2db9365cf891481e3e1322b
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-10 08:57:38 +0200

    ArrayVisualizer comp doesn't need onClick

--- src/components/arrayVisualizer.jsx
+++ src/components/arrayVisualizer.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ArrayBar from "./arrayBar";
 
-const ArrayVisualizer = ({ onClick, elements }) => {
+const ArrayVisualizer = ({ elements }) => {
   const style = {
     height: 500,
     margin: "auto"
   };
 
   return (
-    <div className="row" style={style} onClick={onClick}>
+    <div className="row" style={style}>
       <div className="row" style={{ margin: "auto" }}>
         {elements.map((element, index) => (
           <ArrayBar maxValue={500} element={element} barsNumber={elements.length} key={index} />
