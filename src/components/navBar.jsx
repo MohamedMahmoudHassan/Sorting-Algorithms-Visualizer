7b84d2d0fa286ccc2ba023b8f23e9e5136f1e70f
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-10 14:36:23 +0200

    Navbar shadow

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NavBar = () => {
   return (
     <div>
-      <nav className="navbar-dark bg-default navbar navbar-expand-lg">
+      <nav className="navbar-dark bg-default navbar navbar-expand-lg shadow">
         <div className="navbar-brand" style={{ fontSize: 20, margin:"auto" }}>
           Sorting Algorithms Visualizer
         </div>
