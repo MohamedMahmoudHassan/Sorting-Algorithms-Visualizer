5ea36a6f848afc5e8099bd685c422d25d632e9a4
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-10 16:57:22 +0200

    Center icons on Visualization Control Bar

--- src/components/visualizationControlBar.jsx
+++ src/components/visualizationControlBar.jsx
@@ -3,22 +3,31 @@ import VisualizationButton from "./visualizationButton";
 
 const VisualizationControlBar = ({ handleSortStart }) => {
   return (
-    <div className="py-5 bg-secondary" style={{ margin: "auto" }}>
+    <div className="py-1 bg-default">
       <div className="container">
-        <VisualizationButton
-          handleClick={handleSortStart}
-          role="step-backward"
-          size={1}
-          title="Previous Step"
-        />
-        <VisualizationButton handleClick={handleSortStart} role="play" size={2} title="Run" />
-        <VisualizationButton
-          handleClick={handleSortStart}
-          role="step-forward"
-          size={1}
-          title="Next Step"
-        />
-        <VisualizationButton handleClick={handleSortStart} role="stop" size={1} />
+        <div className="row">
+          <div className="mt-4 mt-md-0 col-md-auto" style={{ margin: "auto" }}>
+            <VisualizationButton
+              handleClick={handleSortStart}
+              role="step-backward"
+              size={1}
+              title="Previous Step"
+            />
+            <VisualizationButton handleClick={handleSortStart} role="play" size={2} title="Run" />
+            <VisualizationButton
+              handleClick={handleSortStart}
+              role="step-forward"
+              size={1}
+              title="Next Step"
+            />
+            <VisualizationButton
+              handleClick={handleSortStart}
+              role="stop"
+              size={1}
+              title="Rest Array"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
