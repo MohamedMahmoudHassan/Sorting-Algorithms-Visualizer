5763f29d25623d72471132173ae0de42956c74da
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-10 19:19:10 +0200

    ArrayVisualizer style needed again(fix zoom bug)

--- src/components/arrayVisualizer.jsx
+++ src/components/arrayVisualizer.jsx
@@ -11,9 +11,10 @@ const ArrayVisualizer = ({
   isSorting,
   isBeforeSort
 }) => {
+  const style = { width: 1000, height: 500, margin: "auto" };
   return (
     <div>
-      <div className="row" style={{ height: 500, margin: "auto" }}>
+      <div className="row" style={style}>
         <div className="row" style={{ margin: "auto" }}>
           {elements.map((element, index) => (
             <ArrayBar maxValue={500} element={element} barsNumber={elements.length} key={index} />
