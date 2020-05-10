1c43793bb7534cbf125f111c6724cebe6eca7e0f
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-10 14:44:10 +0200

    Add stop - step front/back icons

--- src/components/visualizationControlBar.jsx
+++ src/components/visualizationControlBar.jsx
@@ -3,9 +3,22 @@ import VisualizationButton from "./visualizationButton";
 
 const VisualizationControlBar = ({ handleSortStart }) => {
   return (
-    <div className="py-5 bg-secondary">
+    <div className="py-5 bg-secondary" style={{ margin: "auto" }}>
       <div className="container">
-        <VisualizationButton handleClick={handleSortStart} role={"play"} size={5} />
+        <VisualizationButton
+          handleClick={handleSortStart}
+          role="step-backward"
+          size={1}
+          title="Previous Step"
+        />
+        <VisualizationButton handleClick={handleSortStart} role="play" size={2} title="Run" />
+        <VisualizationButton
+          handleClick={handleSortStart}
+          role="step-forward"
+          size={1}
+          title="Next Step"
+        />
+        <VisualizationButton handleClick={handleSortStart} role="stop" size={1} />
       </div>
     </div>
   );
