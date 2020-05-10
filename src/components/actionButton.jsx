f9ad3bb7d1d860d21c394d0079f3e7d81dc6c5b0
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-10 05:47:01 +0200

    Add margin to action buttons

--- src/components/actionButton.jsx
+++ src/components/actionButton.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const ActionButton = ({ handleClick, label, ...rest }) => {
   return (
-    <button className="btn btn-primary" onClick={handleClick} {...rest}>
-      {label}
-    </button>
+    <div style={{ marginTop: 30 }}>
+      <button className="btn btn-primary" onClick={handleClick} {...rest}>
+        {label}
+      </button>
+    </div>
   );
 };
 
